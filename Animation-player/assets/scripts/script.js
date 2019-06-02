7af603699c358067f272a5a30223bec5d9c8c2f4
Anton Ziuzikov <zuzikov.anton@gmail.com>
2019-06-02 22:23:14 +0300

    feat: add animation button and logic

--- Animation-player/assets/scripts/script.js
+++ Animation-player/assets/scripts/script.js
@@ -184,9 +184,49 @@ function setAdditionalInterface() {
       }
     });
   }
+  function findNextCanvas(current) {
+    if (current.nextElementSibling) {
+      return current.nextElementSibling;
+    }
+    return document.getElementsByClassName('main__workspace__canvas_wrapper')[0].firstElementChild;
+  }
+  function runAnimation(interval) {
+    const animationId = document.getElementsByClassName('footer_button_animate')[0];
+    if (+animationId.getAttribute('animated') !== 0) {
+      cancelAnimationFrame((+animationId.getAttribute('animated')));
+      animationId.setAttribute('animated', '0');
+      return;
+    }
+    let start = performance.now();
+    const canvasList = document.getElementsByClassName('main__workspace__canvas_wrapper')[0];
+    let currentCanvas = canvasList.getElementsByClassName('current')[0];
+    let nextCanvas = findNextCanvas(currentCanvas);
+    console.log(start, start);
+    animationId.setAttribute('animated', requestAnimationFrame(function animate(time) {
+      const timePassed = time - start;
+      if (timePassed >= interval) {
+        start = time;
+        console.log(timePassed);
+        currentCanvas.className = currentCanvas.className.replace(' current', '');
+        nextCanvas.classList += ' current';
+        currentCanvas = nextCanvas;
+        nextCanvas = findNextCanvas(currentCanvas);
+        animationId.setAttribute('animated', requestAnimationFrame(animate));
+      } else {
+        animationId.setAttribute('animated', requestAnimationFrame(animate));
+      }
+    }));
+  }
+  function animateCanvas() {
+    const animateButton = document.getElementsByClassName('footer_button_animate')[0];
+    animateButton.addEventListener('click', () => {
+      runAnimation(400);
+    });
+  }
 
   return (function () {
     resetLocalStorage();
+    animateCanvas();
   }());
 }
 
