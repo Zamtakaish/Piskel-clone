8b315d6a90993cd556501800602fc4a4d1252848
Anton Ziuzikov <zuzikov.anton@gmail.com>
2019-06-29 18:41:38 +0300

    feat: apply rescaling to canvas

---
+++ Piskel-clone/assets/scripts/controllers/misc/rescaleCanvas.js
@@ -0,0 +1,24 @@
+export default function rescaleCanvas(direction) {
+  const canvas = document.getElementsByClassName('main__workspace__canvas_wrapper')[0].getElementsByClassName('current')[0];
+  const canvasTemp = document.getElementsByClassName('main__workspace__canvas_temp')[0];
+  const preview = document.getElementsByClassName('main__workspace__preview')[0];
+  const canvasContext = canvas.getContext('2d');
+  const tempContext = canvasTemp.getContext('2d');
+  const previewContext = preview.getContext('2d');
+  canvasTemp.width = canvas.width;
+  canvasTemp.height = canvas.height;
+  tempContext.imageSmoothingEnabled = false;
+  tempContext.mozImageSmoothingEnabled = false;
+  tempContext.webkitImageSmoothingEnabled = false;
+  tempContext.msImageSmoothingEnabled = false;
+  if (direction) {
+    tempContext.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, (+canvas.width / 2), (+canvas.height / 2));
+  } else {
+    tempContext.drawImage(canvas, 0, 0, (+canvas.width / 2), (+canvas.height / 2), 0, 0, canvas.width, canvas.height);
+  }
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+  canvasContext.drawImage(canvasTemp, 0, 0, canvas.width, canvas.height, 0, 0, 32, 32);
+  previewContext.clearRect(0, 0, canvas.width, canvas.height);
+  previewContext.drawImage(canvasTemp, 0, 0, canvas.width, canvas.height, 0, 0, 32, 32);
+  tempContext.clearRect(0, 0, canvas.width, canvas.height);
+}
--- Piskel-clone/assets/scripts/controllers/scalingController.js
+++ Piskel-clone/assets/scripts/controllers/scalingController.js
@@ -1,14 +1,22 @@
+import rescaleCanvas from './misc/rescaleCanvas';
+
 export default function scalingController() {
   const wrapper = document.getElementsByClassName('main__workspace__size-scale')[0];
 
   function rescaleHandler(event) {
-      const currentSize = wrapper.getElementsByClassName('current')[0];
+    const currentSize = wrapper.getElementsByClassName('current')[0];
     const targetButton = event.target;
     if ((targetButton !== wrapper) && (targetButton !== currentSize)) {
       currentSize.className = currentSize.className.replace(' current', '');
       targetButton.className += ' current';
-      console.log()
-      wrapper.setAttribute('scale', targetButton.id.slice(1));
+      const currentScale = wrapper.getAttribute('scale');
+      const nextScale = targetButton.id.slice(1);
+      if (+currentScale < +nextScale) {
+        rescaleCanvas(true);
+      } else {
+        rescaleCanvas(false);
+      }
+      wrapper.setAttribute('scale', nextScale);
     }
   }
 
