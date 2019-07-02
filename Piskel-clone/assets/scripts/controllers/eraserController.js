ea9858d103ff3fa6d7e55cb95917385458ed826e
Anton Ziuzikov <zuzikov.anton@gmail.com>
2019-07-02 23:57:37 +0300

    fix: fix eraser tool to work in all frames

--- Piskel-clone/assets/scripts/controllers/eraserController.js
+++ Piskel-clone/assets/scripts/controllers/eraserController.js
@@ -2,14 +2,14 @@ import checkButton from './misc/checkButton';
 
 export default function eraserController() {
   const workspace = document.getElementsByClassName('main__workspace__canvas_wrapper')[0];
-  const canvas = workspace.getElementsByClassName('current')[0];
-  const canvasContext = canvas.getContext('2d');
   const scaleSize = Math.floor(getComputedStyle(workspace).width.slice(0, -2) / 32);
 
   function eraseHandler(event) {
+    const canvas = workspace.getElementsByClassName('current')[0];
+    const canvasContext = canvas.getContext('2d');
     if (checkButton(3) && (canvas.classList.contains('active'))) {
       canvasContext.clearRect(Math.floor(event.offsetX / scaleSize), Math.floor(event.offsetY / scaleSize), 1, 1);
     }
   }
-  return canvas.addEventListener('mousemove', eraseHandler);
+  return workspace.addEventListener('mousemove', eraseHandler);
 }
