d01cc5f921a1fc461bcbde5771f86a3e48aa1ad8
Anton Ziuzikov <zuzikov.anton@gmail.com>
2019-07-09 01:42:14 +0300

    feat: implement fps control

--- Piskel-clone/assets/scripts/controllers/utility buttons/fpsController.js
+++ Piskel-clone/assets/scripts/controllers/utility buttons/fpsController.js
@@ -1,6 +1,21 @@
+import renderAnimation from '../../rendering/renderAnimation';
+
 export default function fpsController() {
   const fpsSlider = document.getElementById('fps');
+  const fpsHihlight = document.getElementsByClassName('footer_button_animate-rate_highlight')[0];
+
+  fpsSlider.addEventListener('mousedown', () => {
+    fpsHihlight.style.visibility = 'visible';
+    fpsHihlight.innerHTML = `${fpsSlider.value} fps`;
+  });
+
   fpsSlider.addEventListener('input', () => {
     fpsSlider.setAttribute('value', fpsSlider.value);
+    fpsHihlight.innerHTML = `${fpsSlider.value} fps`;
+    renderAnimation();
+  });
+
+  fpsSlider.addEventListener('mouseup', () => {
+    fpsHihlight.style.visibility = 'hidden';
   });
 }
